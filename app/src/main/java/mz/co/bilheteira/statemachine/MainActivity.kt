1d729d7c8ceaa664d9d3af3ecda6fbb8b09caa10
Anselmo Alexandre <anselmo@snappmobile.io>
2023-06-22 09:22:35 +0200

    Initial commit

---
+++ app/src/main/java/mz/co/bilheteira/statemachine/MainActivity.kt
@@ -0,0 +1,51 @@
+package mz.co.bilheteira.statemachine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mz.co.bilheteira.statemachine.ui.theme.StateMachineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StateMachineTheme {
+                // A surface container using the 'background' color from the theme
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        color = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.primary
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StateMachineTheme {
+        Greeting("Android")
+    }
+}
