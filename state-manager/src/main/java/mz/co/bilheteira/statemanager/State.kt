8ee3968264fb6db68880e03b213f4ba9f13aa457
Anselmo Alexandre <anselmo@snappmobile.io>
2023-06-27 23:17:34 +0200

    Added the core MVI classes. Added State, Action, Reducer and Store which is the state container

---
+++ state-manager/src/main/java/mz/co/bilheteira/statemanager/State.kt
@@ -0,0 +1,9 @@
+package mz.co.bilheteira.statemanager
+
+/**
+ * [State] is used to clearly represent any class that represent a view state
+ * for a given screen
+ *
+ * This is basically any data class that defines the view for a given screen
+ */
+interface State
