edaf8e82684d8a24955022375da22efa733ce0a4
Anselmo Alexandre <anselmo@snappmobile.io>
2023-07-05 17:39:06 +0200

    Created database related classes

---
+++ storage/src/main/java/mz/co/bilheteira/storage/dao/LocationDao.kt
@@ -0,0 +1,18 @@
+package mz.co.bilheteira.storage.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import mz.co.bilheteira.storage.entity.LocationEntity
+
+
+@Dao
+interface LocationDao {
+    @Query("SELECT * FROM location")
+    fun getLocations(): Flow<List<LocationEntity>>
+
+    @Insert(LocationEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertLocation(locationEntity: LocationEntity)
+}
